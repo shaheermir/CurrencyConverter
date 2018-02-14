734b5b6072be2f54fdbc3a8332db97b905086a2d
Shaheer Mir <shaheermir0@gmail.com>
2018-02-14 12:58:59 -0500

    Added Options screen.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import EStyleSheet from 'react-native-extended-stylesheet'
+
 import Home from './screens/Home'
 import CurrencyList from './screens/CurrencyList'
-import EStyleSheet from 'react-native-extended-stylesheet'
+import Options from './screens/Options'
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -13,4 +15,4 @@ EStyleSheet.build({
   // $outline: 1
 })
 
-export default () => <CurrencyList />
+export default () => <Options />
