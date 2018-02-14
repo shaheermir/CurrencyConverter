c1270a0fa9e867e432e062d90399360e8ca82450
Shaheer Mir <shaheermir0gmail.com>
2018-02-14 15:07:45 -0500

    Finished the Themes screen.

---
+++ app/screens/Themes.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import { ScrollView, StatusBar } from 'react-native'
+
+import { ListItem, Seperator } from '../components/List'
+
+const styles = EStyleSheet.create({
+  $blue: '$primaryBlue',
+  $orange: '$primaryOrange',
+  $green: '$primaryGreen',
+  $purple: '$primaryPurple'
+})
+
+class Themes extends React.Component {
+  handleThemePress = color => {
+    console.log(`You picked theme color ${color}`)
+  }
+
+  render () {
+    const { $blue, $orange, $green, $purple } = styles
+    return (
+      <ScrollView>
+        <StatusBar barStyle='default' translucent={false} />
+        <ListItem
+          text='Blue'
+          onPress={() => this.handleThemePress($blue)}
+          selected
+          checkmark={false}
+          iconBackground={$blue}
+        />
+        <Seperator />
+        <ListItem
+          text='Orange'
+          onPress={() => this.handleThemePress($orange)}
+          selected
+          checkmark={false}
+          iconBackground={$orange}
+        />
+        <Seperator />
+        <ListItem
+          text='Green'
+          onPress={() => this.handleThemePress($green)}
+          selected
+          checkmark={false}
+          iconBackground={$green}
+        />
+        <Seperator />
+        <ListItem
+          text='Purple'
+          onPress={() => this.handleThemePress($purple)}
+          selected
+          checkmark={false}
+          iconBackground={$purple}
+        />
+        <Seperator />
+      </ScrollView>
+    )
+  }
+}
+
+export default Themes
